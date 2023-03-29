99813baaf3fdec5627f1b64065d9a0e225cf5528
jhony2022 <thecapoflash@hotmail.com>
2023-03-28 23:19:20 -0300

    cambios con express

--- servidor.js
+++ servidor.js
@@ -12,15 +12,25 @@ const products = new ProductManager();
 
 app.get("/products", async (req, res) => {
 
-    const showGames = await products.getProducts()
+    let showGames = await products.getProducts()
 
-    let limit = (req.query.limit);
+    let limit = parseInt(req.query.limit);
     if(limit > 0){
         const limitProducts = showGames.splice(0, limit);
         return res.send(limitProducts)
     }
     return res.send(showGames);
 });
+
+
+app.get("/products/:id", async (req, res) => {
+
+    let showGames = await products.getProducts()
+    let id = parseInt(req.params.id);
+    let gameById = showGames.find(juego => juego.id === id)
+    res.send(gameById)
+
+})
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
